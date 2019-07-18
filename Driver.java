21d69a0e39673bb8f0ea9cd42ee7fcb8a2c2c11b
AnuragJain <anuragjaindel@yahoo.com>
2019-07-18 07:17:51 +0000

    The first commit to Master Branch

---
+++ Driver.java
@@ -0,0 +1,72 @@
+package regJava;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Driver {
+	
+	public static WebDriver driver; 
+	public static String browser;
+	public static Properties prop = new Properties();
+	public static String osType;
+	public static String url;
+	
+	public void Login() throws IOException {
+		
+		FileInputStream fis = new FileInputStream(
+				System.getProperty("user.dir") + "/src/main/resources/Regs.properties");
+		
+		prop.load(fis);
+		browser = prop.getProperty("browser");
+		osType = prop.getProperty("osType");
+		url = prop.getProperty("login");
+
+		if (browser.equalsIgnoreCase("firefox")) {
+			if (osType.equalsIgnoreCase("Windows")) { // Set Path for the executable file
+				System.setProperty("webdriver.gecko.driver", "src/main/resources/drivers/geckodriver.exe");
+				driver = new FirefoxDriver();
+				WebDriverWait wait = new WebDriverWait(driver, 50);
+				driver.manage().window().maximize();
+				driver.get(url);
+				driver.findElement(By.xpath("//input[@value='Login']/ancestor::div[2]//div[5]//a[2]/h2")).click();
+				wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[contains(text(),'Register')]")));
+			} else if (osType.equalsIgnoreCase("Linux")) {
+				System.setProperty("webdriver.firefox.marionette", "src/main/resources/drivers/geckodriver.exe");
+				driver = new FirefoxDriver();
+				WebDriverWait wait = new WebDriverWait(driver, 50);
+				driver.manage().window().maximize();
+				driver.get(url);
+				driver.findElement(By.xpath("//input[@value='Login']/ancestor::div[2]//div[5]//a[2]/h2")).click();
+				wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[contains(text(),'Register')]")));
+			}
+		}
+
+		else if (browser.equalsIgnoreCase("chrome")) {
+
+			if (osType.equalsIgnoreCase("Windows")) {
+				System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe");
+				driver = new ChromeDriver();
+				WebDriverWait wait = new WebDriverWait(driver, 50);
+				driver.manage().window().maximize();
+				driver.get(url);
+				driver.findElement(By.xpath("//input[@value='Login']/ancestor::div[2]//div[5]//a[2]/h2")).click();
+				wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[contains(text(),'Register')]")));
+			} else {
+				throw new IllegalArgumentException("Could not find supported browser: " + browser);
+			}
+		}
+	}
+
+public void close() {
+driver.close();
+}
+}
